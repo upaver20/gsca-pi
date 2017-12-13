fbe68fb4858f8eb9c3157144a5b15baaa04571ee
upaver20 <upa.ver.2.0@gmail.com>
2017-12-14 02:36:43 +0900

    Compliant with pep8

--- api.py
+++ api.py
@@ -6,11 +6,13 @@ import pymongo
 from waitress import serve
 from datetime import datetime
 
+
 class DateTimeSupportJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime):
             return o.isoformat()
         return super(DateTimeSupportJSONEncoder, self).default(o)
+
 
 class HelloResource(object):
 
@@ -22,15 +24,17 @@ class HelloResource(object):
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(msg)
 
+
 class UserList(object):
     def on_get(self, req, resp):
         client = pymongo.MongoClient()
         db = client['r6status']
         recent = db['recent']
-        msg = list(recent.find({}, {'_id': 0,'id':1,'date':1}))
+        msg = list(recent.find({}, {'_id': 0, 'id': 1, 'date': 1}))
 
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder )
+        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder)
+
 
 class UserData(object):
     def on_get(self, req, resp, id):
@@ -38,18 +42,19 @@ class UserData(object):
         db = client['r6status']
         old = db['old']
 
-        msg = list( old.find({'id':id},{'_id':0}).sort('date', pymongo.DESCENDING))
+        msg = list(old.find({'id': id}, {'_id': 0})
+                   .sort('date', pymongo.DESCENDING))
 
         if msg:
             resp.status = falcon.HTTP_200
-            resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder )
+            resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder)
 
         else:
             errmsg = {
-            "message": "ID was not valid."
+                "message": "ID was not valid."
             }
             resp.status = falcon.HTTP_404
-            resp.body = json.dumps(errmsg, cls=DateTimeSupportJSONEncoder )
+            resp.body = json.dumps(errmsg, cls=DateTimeSupportJSONEncoder)
 
     def on_post(self, req, resp, id):
         client = pymongo.MongoClient()
@@ -57,13 +62,14 @@ class UserData(object):
         userdb = db['user']
         date = datetime.utcnow()
 
-        userdb.update({"id":id},{'$set':{"deathcount":0}},upsert=True)
-        userdb.update({"id":id},{'$set':{"date":date}},upsert=True)
+        userdb.update({"id": id}, {'$set': {"deathcount": 0}}, upsert=True)
+        userdb.update({"id": id}, {'$set': {"date": date}}, upsert=True)
         msg = {
             "message": "success to add DataBase"
         }
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder )
+        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder)
+
 
 app = falcon.API()
 app.add_route("/hello", HelloResource())

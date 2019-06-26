94ea295b75346f1e5fece3569fb7897b336018aa
upaver20 <upaver20@upaver20.com>
2019-06-27 05:10:15 +0900

    fix error

--- api.py
+++ api.py
@@ -32,17 +32,22 @@ class UserList(object):
         msg = list(recent.find({}, {'_id': 0, 'id': 1, 'date': 1}))
 
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder)
+        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
 
 
 class UserData(object):
-    def on_get(self, req, resp,id):
+    def on_get(self, req, resp, id):
         client = pymongo.MongoClient()
         db = client['r6status']
         old = db['old']
+        id2uid = db['id2uid']
 
-        user_data = list(old.find({'id': id}, {'_id': 0})
-                   .sort('date', pymongo.DESCENDING)) 
+        # prm = req.params()
+        # print(prm['count'])
+
+        uid = list(id2uid.find({'id': id}, {'_id': 0})
+                   .sort('date', pymongo.DESCENDING))[0]["uid"]
+        user_data = list(old.find({'uid': uid}, {'_id': 0}).sort('date', pymongo.DESCENDING))
 
         count = req.get_param_as_int('count')
 
@@ -55,31 +60,35 @@ class UserData(object):
 
         if msg:
             resp.status = falcon.HTTP_200
-            resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder)
+            resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
 
         else:
             errmsg = {
                 "message": "ID was not valid."
             }
             resp.status = falcon.HTTP_404
-            resp.body = json.dumps(errmsg, cls=DateTimeSupportJSONEncoder)
+            resp.body = json.dumps(errmsg, cls=DateTimeSupportJSONEncoder, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
 
     def on_post(self, req, resp, id):
         client = pymongo.MongoClient()
         db = client['r6status']
-        userdb = db['user']
+        dead_iddb = db['dead_id']
         date = datetime.utcnow()
 
-        userdb.update({"id": id}, {'$set': {"deathcount": 0}}, upsert=True)
-        userdb.update({"id": id}, {'$set': {"date": date}}, upsert=True)
+        dead_id.update({"id": id}, { '$set': {"date": date}, '$inc': {"deathcount": 1}}, upsert=True)
+
         msg = {
             "message": "success to add DataBase"
         }
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder)
+        resp.body = json.dumps(msg, cls=DateTimeSupportJSONEncoder, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+
+    class CORSMiddleware:
+        def process_request(self, req, resp):
+            resp.set_header('Access-Control-Allow-Origin', '*')
 
 
-app = falcon.API()
+app = falcon.API(middleware=[CORSMiddleware()])
 app.add_route("/hello", HelloResource())
 app.add_route("/userlist", UserList())
 app.add_route("/userdata/{id}", UserData())
